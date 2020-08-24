3868bba50a6545d498ad3fb06cbd5f779afda6a9
Jorge <jorgediazok@gmail.com>
2020-08-24 19:43:11 -0300

    creating components of the UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+
+import Home from './containers/Home/index';
+import Header from './components/Header/index';
+import Hero from './components/Hero/index';
+
 import './App.css';
 
 function App() {
-  return <div className="App">Jorge Diaz DV</div>;
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Hero/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from '../UI/Card/index';
+import './style.css';
+
+const Hero = (props) => {
+  return (
+    <div>
+      <Card>My name is Jorge</Card>
+    </div>
+  );
+};
+
+export default Hero;
