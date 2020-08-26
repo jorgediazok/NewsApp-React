33610d2530f038e23baacbf665b1a71466c5de07
Jorge <jorgediazok@gmail.com>
2020-08-26 01:51:35 -0300

    last changes before sleeping. I made changes in the css

--- src/components/Blogpost/index.js
+++ src/components/Blogpost/index.js
@@ -5,46 +5,47 @@ import blogPost from '../../data/blog.json';
 import './style.css';
 
 const Blogpost = (props) => {
-  const [post, setPost] = useState({});
+  const [post, setPost] = useState({
+    id: '',
+    blogCategory: '',
+    blogTitle: '',
+    postedOn: '',
+    author: '',
+    blogImage: '',
+    blogText: '',
+  });
+
   const [postId, setPostId] = useState('');
 
   useEffect(() => {
     const postId = props.match.params.postId;
     const post = blogPost.data.find((post) => post.id == postId);
+    // @ts-ignore
     setPost(post);
     setPostId(postId);
   }, [post, props.match.params.postId]);
+
+  if (post.blogImage == '') return null;
 
   return (
     <div className="blogPostContainer">
       <Card>
         <div className="blogHeader">
-          <span className="blogCategory">Featured</span>
-          <h1 className="postTitle">
-            {
-              // @ts-ignore
-              post.blogTitle
-            }
-          </h1>
+          <span className="blogCategory">{post.blogCategory}</span>
+          <h1 className="postTitle">{post.blogTitle}</h1>
           <span className="postedBy">
-            posted on August 25, 2020 by Jorge Dv
+            posted on {post.postedOn} by {post.author}
           </span>
         </div>
         <div className="postImageContainer">
           <img
-            // @ts-ignore
-            src={require('../../blogPostImages/memories-from.jpg')}
+            src={require('../../blogPostImages/' + post.blogImage)}
             alt="memo"
           />
         </div>
         <div className="postContent">
-          <h3>Post Title</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate
-            doloremque beatae voluptatibus ea voluptas, possimus a architecto
-            sed tenetur voluptates quaerat soluta enim at commodi perferendis
-            officiis sit deserunt unde.
-          </p>
+          <h3>{post.blogTitle}</h3>
+          <p>{post.blogText}</p>
         </div>
       </Card>
     </div>
